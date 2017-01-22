78a32f805431529a15cafee80f29c15f2a20d8ea
alisayo <myaddress-me@163.com>
2017-01-22 11:03:59 +0800

    ysback
    基于bootstrap的后台框架

---
+++ js/base.js
@@ -0,0 +1,426 @@
+ // 菜单数据
+ var _menus = {
+     basic: [{
+         "id": "01",
+         "name": "data table",
+         "icon": "icon-tree glyphicon glyphicon-list-alt",
+         "menus": [{
+             "id": "0101",
+             "name": "table",
+             "icon": "icon-nav",
+             "menus": [{
+                 "id": "010301",
+                 "name": "base table",
+                 "icon": "icon-nav",
+                 "url": "table-base.html"
+             }, {
+                 "id": "010302",
+                 "name": "菜单小项子菜单2",
+                 "icon": "icon-nav",
+                 "url": "html/nwxmdzb.html"
+             }]
+         }, {
+             "id": "0102",
+             "name": "数据分析功能",
+             "icon": "icon-nav",
+             "menus": [{
+                 "id": "010301",
+                 "name": "数据分析功能子菜单",
+                 "icon": "icon-nav",
+                 "url": "html/nwjetjxxb.html"
+             }]
+         }, {
+             "id": "0103",
+             "name": "文档管理功能",
+             "icon": "icon-nav",
+             "menus": [{
+                 "id": "010301",
+                 "name": "文档管理功能子菜单",
+                 "icon": "icon-nav",
+                 "url": "html/nwjetjxxb.html"
+             }]
+         }]
+     }, {
+         "id": "02",
+         "icon": "icon-tree glyphicon glyphicon-user",
+         "name": "个人信息",
+         "menus": [{
+             "id": "0201",
+             "name": "个人信息管理功能",
+             "icon": "icon-nav",
+             "menus": [{
+                 "id": "020101",
+                 "name": "个人信息管理功能子菜单",
+                 "icon": "icon-nav",
+                 "url": "html/nwjetjxxb.html"
+             }]
+         }, {
+             "id": "0202",
+             "name": "代办事务功能",
+             "icon": "icon-nav",
+             "menus": [{
+                 "id": "020201",
+                 "name": "代办事务功能子菜单",
+                 "icon": "icon-nav",
+                 "url": "html/nwjetjxxb.html"
+             }]
+         }]
+     }, {
+         "id": "03",
+         "icon": "icon-tree glyphicon glyphicon-cog",
+         "name": "系统管理",
+         "menus": [{
+             "id": "0301",
+             "name": "权限管理",
+             "icon": "icon-nav",
+             "menus": [{
+                 "id": "030101",
+                 "name": "权限管理子菜单",
+                 "icon": "icon-nav",
+                 "url": "html/nwjetjxxb.html"
+             }]
+         }, {
+             "id": "0302",
+             "name": "接口管理",
+             "icon": "icon-nav",
+             "menus": [{
+                 "id": "020201",
+                 "name": "接口管理子菜单",
+                 "icon": "icon-nav",
+                 "url": "html/nwjetjxxb.html"
+             }]
+         }]
+     }],
+     basic2: [{
+         "id": "01",
+         "icon": "icon-tree icon-tree-gnyy",
+         "name": "功能应用",
+         "menus": [{
+                 "id": "0101",
+                 "name": "表单填报功能",
+                 "icon": "icon-nav",
+                 "menus": [{
+                     "id": "010301",
+                     "name": "表单填报功能子菜单",
+                     "icon": "icon-nav",
+                     "url": "html/nwjetjxxb.html"
+                 }, {
+                     "id": "010302",
+                     "name": "表单填报功能子菜单2",
+                     "icon": "icon-nav",
+                     "url": "html/nwxmdzb.html"
+                 }]
+             }, {
+                 "id": "0102",
+                 "name": "数据分析功能",
+                 "icon": "icon-nav",
+                 "menus": [{
+                     "id": "010301",
+                     "name": "数据分析功能子菜单",
+                     "icon": "icon-nav",
+                     "url": "html/nwjetjxxb.html"
+                 }]
+             },
+             //
+             {
+                 "id": "0103",
+                 "name": "趋势预测功能",
+                 "icon": "icon-nav",
+                 "menus": [{
+                     "id": "010301",
+                     "name": "趋势预测功能子菜单",
+                     "icon": "icon-nav",
+                     "url": "html/nwjetjxxb.html"
+                 }]
+
+             },
+
+             //
+             {
+                 "id": "0103",
+                 "name": "文档管理功能",
+                 "icon": "icon-nav",
+                 "menus": [{
+                     "id": "010301",
+                     "name": "文档管理功能子菜单",
+                     "icon": "icon-nav",
+                     "url": "html/nwjetjxxb.html"
+                 }]
+             }
+         ]
+     }, {
+         "id": "02",
+         "icon": "icon-tree icon-tree-grxx",
+         "name": "个人信息",
+         "menus": [{
+             "id": "0201",
+             "name": "个人信息管理功能",
+             "icon": "icon-nav",
+             "menus": [{
+                 "id": "020101",
+                 "name": "个人信息管理功能子菜单",
+                 "icon": "icon-nav",
+                 "url": "html/nwjetjxxb.html"
+             }]
+         }, {
+             "id": "0202",
+             "name": "代办事务功能",
+             "icon": "icon-nav",
+             "menus": [{
+                 "id": "020201",
+                 "name": "代办事务功能子菜单",
+                 "icon": "icon-nav",
+                 "url": "html/nwjetjxxb.html"
+             }]
+         }]
+     }, {
+         "id": "03",
+         "icon": "icon-tree icon-tree-xtgl",
+         "name": "系统管理",
+         "menus": [{
+             "id": "0301",
+             "name": "权限管理",
+             "icon": "icon-nav",
+             "menus": [{
+                 "id": "030101",
+                 "name": "权限管理子菜单",
+                 "icon": "icon-nav",
+                 "url": "html/nwjetjxxb.html"
+             }]
+         }, {
+             "id": "0302",
+             "name": "接口管理",
+             "icon": "icon-nav",
+             "menus": [{
+                 "id": "020201",
+                 "name": "接口管理子菜单",
+                 "icon": "icon-nav",
+                 "url": "html/nwjetjxxb.html"
+             }]
+         }]
+     }],
+     basic3: [{
+         "id": "01",
+         "icon": "icon-sys",
+         "name": "功能应用",
+         "menus": [{
+                 "id": "0101",
+                 "name": "表单填报功能",
+                 "icon": "icon-nav",
+                 "menus": [{
+                     "id": "010301",
+                     "name": "表单填报功能子菜单",
+                     "icon": "icon-nav",
+                     "url": "html/nwjetjxxb.html"
+                 }]
+             }, {
+                 "id": "0102",
+                 "name": "数据分析功能",
+                 "icon": "icon-nav",
+                 "menus": [{
+                     "id": "010301",
+                     "name": "数据分析功能子菜单",
+                     "icon": "icon-nav",
+                     "url": "html/nwjetjxxb.html"
+                 }]
+             },
+             //
+             {
+                 "id": "0103",
+                 "name": "趋势预测功能",
+                 "icon": "icon-nav",
+                 "menus": [{
+                     "id": "010301",
+                     "name": "趋势预测功能子菜单",
+                     "icon": "icon-nav",
+                     "url": "html/nwjetjxxb.html"
+                 }]
+
+             },
+
+             //
+             {
+                 "id": "0103",
+                 "name": "文档管理功能",
+                 "icon": "icon-nav",
+                 "menus": [{
+                     "id": "010301",
+                     "name": "文档管理功能子菜单",
+                     "icon": "icon-nav",
+                     "url": "html/nwjetjxxb.html"
+                 }]
+             }
+         ]
+     }, {
+         "id": "02",
+         "icon": "icon-sys",
+         "name": "个人信息",
+         "menus": [{
+             "id": "0201",
+             "name": "个人信息管理功能",
+             "icon": "icon-nav",
+             "menus": [{
+                 "id": "020101",
+                 "name": "个人信息管理功能子菜单",
+                 "icon": "icon-nav",
+                 "url": "html/nwjetjxxb.html"
+             }]
+         }, {
+             "id": "0202",
+             "name": "代办事务功能",
+             "icon": "icon-nav",
+             "menus": [{
+                 "id": "020201",
+                 "name": "代办事务功能子菜单",
+                 "icon": "icon-nav",
+                 "url": "html/nwjetjxxb.html"
+             }]
+         }]
+     }],
+     basic4: [{
+         "id": "01",
+         "icon": "icon-sys",
+         "name": "功能应用",
+         "menus": [{
+                 "id": "0101",
+                 "name": "表单填报功能",
+                 "icon": "icon-nav",
+                 "menus": [{
+                     "id": "010301",
+                     "name": "表单填报功能子菜单",
+                     "icon": "icon-nav",
+                     "url": "html/nwjetjxxb.html"
+                 }]
+             }, {
+                 "id": "0102",
+                 "name": "数据分析功能",
+                 "icon": "icon-nav",
+                 "menus": [{
+                     "id": "010301",
+                     "name": "数据分析功能子菜单",
+                     "icon": "icon-nav",
+                     "url": "html/nwjetjxxb.html"
+                 }]
+             },
+             //
+             {
+                 "id": "0103",
+                 "name": "趋势预测功能",
+                 "icon": "icon-nav",
+                 "menus": [{
+                     "id": "010301",
+                     "name": "趋势预测功能子菜单",
+                     "icon": "icon-nav",
+                     "url": "html/nwjetjxxb.html"
+                 }]
+
+             },
+
+             //
+             {
+                 "id": "0103",
+                 "name": "文档管理功能",
+                 "icon": "icon-nav",
+                 "menus": [{
+                     "id": "010301",
+                     "name": "文档管理功能子菜单",
+                     "icon": "icon-nav",
+                     "url": "html/nwjetjxxb.html"
+                 }]
+             }
+         ]
+     }, {
+         "id": "02",
+         "icon": "icon-sys",
+         "name": "个人信息",
+         "menus": [{
+             "id": "0201",
+             "name": "个人信息管理功能",
+             "icon": "icon-nav",
+             "menus": [{
+                 "id": "020101",
+                 "name": "个人信息管理功能子菜单",
+                 "icon": "icon-nav",
+                 "url": "html/nwjetjxxb.html"
+             }]
+         }, {
+             "id": "0202",
+             "name": "代办事务功能",
+             "icon": "icon-nav",
+             "menus": [{
+                 "id": "020201",
+                 "name": "代办事务功能子菜单",
+                 "icon": "icon-nav",
+                 "url": "html/nwjetjxxb.html"
+             }]
+         }]
+     }]
+
+ };
+ var treeStr = "";
+ tree(_menus['basic'], 0);
+ $("#leftTree").html(treeStr);
+
+ //左侧导航加载
+ function tree(data, i) {
+     i++;
+     treeStr += '<ul>';
+     if (typeof data == "object") {
+         $.each(data, function(k, v) {
+             if (v.url != null) {
+                 treeStr += '<li class="ys-tree-level' + i + '"><a target="main" href="' + v.url + '">' + v.name + '</a></li>'
+             } else {
+                 if (i == 1)
+                     treeStr += '<li class="ys-tree-level' + i + '"><h5><span class="' + v.icon + '"></span><span class="yc-tree-text">' + v.name + '</span><span class="ys-arrow glyphicon glyphicon-menu-down"></span></h5>  ';
+                 else treeStr += '<li class="ys-tree-level' + i + '"><h6><span class="yc-tree-text ">' + v.name + '</span></h6>  ';
+                 tree(v.menus, i);
+                 treeStr += '</li>';
+             }
+         })
+     }
+
+     treeStr += '</ul>';
+ }
+
+ // 导航交互
+ $(document).on('click', '.ys-left-maxbar  h5', function(event) {
+     $(this).parent().toggleClass('active').siblings().removeClass('active');
+     $(this).parent().siblings().find('li').removeClass('active');
+     if (!$(this).parent().hasClass('active')) {
+         $(this).next().find("li").removeClass('active');
+     } else {
+         $(this).parent().siblings().removeClass('active');
+     }
+
+ });
+ $(document).on('click', '.ys-left-maxbar  h6', function(event) {
+     $(this).parent().toggleClass('active');
+
+ });
+ $(document).on('click', '.ys-left-bar a', function(event) {
+     $("#leftTree a").parent().removeClass('active');
+     $(this).parent().addClass('active');
+ });
+ $(".j-sidebar-collapse").click(function(event) {
+     $('#sidebar').toggleClass('ys-left-maxbar');
+     $('#sidebar').toggleClass('ys-left-minbar');
+     $("#sidebar li").removeClass('active');
+      $('#mainRight').toggleClass('ys-right-pos');
+ });
+ // 图标模式
+
+ // $(document).on('click', '.ys-left-minbar  h5', function() {
+ //     $(this).parent().toggleClass('active').siblings().removeClass('active');
+ //     $(this).parent().siblings().find('li').removeClass('active');
+ //     if (!$(this).parent().hasClass('active')) {
+ //         $(this).next().find("li").removeClass('active');
+ //     } else {
+ //         $(this).parent().siblings().removeClass('active');
+ //     }
+
+ // });
+
+ $(document).on('click', '.ys-left-minbar  h6', function(event) {
+     $(this).parent().addClass('active').siblings().removeClass('active');
+
+ });
